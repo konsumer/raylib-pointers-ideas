e01d6e96325c0684e3847df0c3e11ee38eaf49bc
David Konsumer <konsumer@jetboystudio.com>
2023-07-09 21:19:36 -0700

    build wasm1

---
+++ src/test-wasm1.c
@@ -0,0 +1,23 @@
+// these will maybe eventually be part of the generated header
+#define WASM_EXPORT __attribute__((visibility("default"))) extern
+#define RLP_EXPORT __attribute__((import_module("env"))) 
+#include "api.h"
+
+
+Texture2D* texBunny;
+
+WASM_EXPORT void InitGame() {
+  rp_InitWindow(800, 450, "raylib-pointers test program");
+  texBunny = (Texture2D*) malloc(sizeof(Texture2D));
+  rp_LoadTexture(texBunny, "wabbit_alpha.png");
+}
+
+WASM_EXPORT void UpdateGame() {
+  rp_BeginDrawing();
+  rp_ClearBackground(rp_RAYWHITE);
+  rp_DrawText("Congrats! You created your first window!", 190, 200, 20, rp_LIGHTGRAY);
+  rp_DrawTexture(texBunny, 100, 100, rp_WHITE);
+  rp_DrawFPS(10, 10);
+  rp_EndDrawing();
+}
+
